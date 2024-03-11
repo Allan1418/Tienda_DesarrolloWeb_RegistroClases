24c776689b7803854c049c3a089c434c9dd14358
Allan1418 <allannb02@gmail.com>
2024-03-11 15:13:19 -0600

    clase 6

---
+++ src/main/java/com/tienda/domain/Producto.java
@@ -0,0 +1,45 @@
+package com.tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+
+@Data
+@Entity
+@Table(name = "Producto")
+public class Producto implements Serializable{
+
+    private static final long serialVersionUID = 1L;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
+    private long idProducto;
+    
+    private String descripcion;
+    private String detalle;
+    private double precio;
+    private int existencias;
+    private String ruta_imagen;
+    private boolean activo;
+
+    public Producto(long idProducto, String descripcion, String detalle, double precio, int existencias, String ruta_imagen, boolean activo) {
+        this.idProducto = idProducto;
+        this.descripcion = descripcion;
+        this.detalle = detalle;
+        this.precio = precio;
+        this.existencias = existencias;
+        this.ruta_imagen = ruta_imagen;
+        this.activo = activo;
+    }
+    
+    
+
+    public Producto() {
+    }
+    
+    
+    
+
+}
